6453eeeeb7a61cf4de3072caecb425dff7dbf7d4
bluedragonz <brian@seoette.com>
2013-08-21 13:25:12 -0400

    Initial commit

---
+++ NameGenerator.class.php
@@ -0,0 +1,19 @@
+class NameGenerator {
+
+  private $_glue;
+
+  public function __construct($glue = ' ') {
+
+    $this->_first = array("Sarah", "Aaliyah", "Anna", "Arianna", "Ellie", "Steven", "Mary");
+    $this->_last = array("Moore", "Martin", "Jackson", "Thompson", "White", "Young", "Hall");
+    $this->_glue = $glue;
+  }
+
+  public function next() {
+    $first = $this->_first[array_rand($this->_first)];
+    $last = $this->_last[array_rand($this->_last)];
+
+    return $first . $this->_glue . $last;
+  }
+
+}
